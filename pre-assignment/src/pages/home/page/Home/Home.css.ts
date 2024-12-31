60efab3a6139666a6c03de94a44165b71be853d3
Parkchaeyeon <ccyy1029@naver.com>
2024-12-31 17:40:54 +0900

    chore: globalStyle 모바일 뷰에 맞게 지정

--- pre-assignment/src/pages/home/page/Home/Home.css.ts
+++ pre-assignment/src/pages/home/page/Home/Home.css.ts
@@ -3,4 +3,9 @@ import { style } from '@vanilla-extract/css';
 
 export const home = style({
   color: globalTheme.color.orange,
+  fontSize: '1.6rem',
+  backgroundColor: globalTheme.color.green,
+  width: '100%',
+  height: '100%',
+  border: '1px solid',
 });
--- pre-assignment/src/styles/global.css.ts
+++ pre-assignment/src/styles/global.css.ts
@@ -1,2 +1,17 @@
+import { globalStyle } from '@vanilla-extract/css';
 import './layers.css';
 import './reset.css';
+
+globalStyle('html', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+globalStyle('#root', {
+  maxWidth: '430px',
+  minWidth: '375px',
+  width: '100dvw',
+  minHeight: '100dvh',
+  fontSize: '62.5%',
+});
