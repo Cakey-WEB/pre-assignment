352f12bf809d3a0056ee23a317f753865813c86c
youtheyeon <yuseoyeon@ewhain.net>
2025-01-02 14:27:40 +0900

    init: Vanilla Extract 설정

---
+++ pre-assignment/src/styles/global.css.ts
@@ -0,0 +1,19 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('html, body', {
+  fontSize: '62.5%',
+  maxWidth: '375px',
+  width: '100%',
+  margin: '0 auto',
+  scrollBehavior: 'smooth',
+});
+
+globalStyle('input, button', {
+  border: 'none',
+  outline: 'none',
+});
+
+globalStyle('a', {
+  color: 'inherit',
+  textDecoration: 'none',
+});
